ac9a385de1e29b3dce765cc37edf630c1ad2cdd9
laurent-g <laurent.georget@gmail.com>
2013-12-24 12:20:05 +0100

    First commit

---
+++ src/main/java/eu/georget/triangulation/App.java
@@ -0,0 +1,13 @@
+package eu.georget.triangulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
